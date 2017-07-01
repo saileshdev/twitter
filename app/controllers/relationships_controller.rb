5e62ec88daa2c810da8422a905f9caf241b42522
Sailesh Dev <saileshdev@gmail.com>
2017-07-02 04:55:09 +0530

    Define create and destroy actions in relationship controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,25 @@
 class RelationshipsController < ApplicationController
+
+  def create
+    relationship = Relationship.new(relationship_params)
+    relationship.follower = current_user
+    # The above is same as @relationship.follower_id = current_user.id 
+
+    flash[:error] = "Couldn't follow" unless relationship.save
+    redirect_to relationship.followed
+  end
+
+  def destroy
+  	relationship = Relationship.find(params[:id])
+  	user = relationship.followed
+  	relationship.destroy
+  	redirect_to user
+  end
+
+private
+
+  def relationship_params
+    params.require(:relationship).permit(:followed_id)
+  end
+
 end
