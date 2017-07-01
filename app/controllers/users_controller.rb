ed5ca1c812505a185cc04ed3b87d21924e2cc359
Sailesh Dev <saileshdev@gmail.com>
2017-07-02 01:32:53 +0530

    Add new and create actions in User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,19 @@ def new
 	@user = User.new
 end
 
+def create
+	@user = User.new(user_params)
+	if @user.save
+		redirect_to @user, notice: "Thank you for signing up for Twitter!"
+	else
+		render 'new'
+end
+
 private
 
 def user_params
-  params.require(:user).permit()
+  params.require(:user).permit(:email, :name, :username, :password, 
+  	:password_confirmation)
 end
 
 end
