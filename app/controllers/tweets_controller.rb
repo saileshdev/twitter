7ee52fdbab3a97a9c97d166bab0a491187bb48fb
Sailesh Dev <saileshdev@gmail.com>
2017-07-02 02:59:08 +0530

    Define create action in tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,18 @@
 class TweetsController < ApplicationController
+
+  def create
+    tweet = Tweet.new(tweet_params)
+    tweet.user_id = current_user.id
+
+    flash[:error] = "Your Tweet was over 140 characters" unless tweet.save
+    redirect_to root_url 
+       
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+
 end
