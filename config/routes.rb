bc547b3fc06dd78456bdb5e7bf0715224e186cbe
Sailesh Dev <saileshdev@gmail.com>
2017-07-02 02:22:07 +0530

    Create tweet resource and add a default scope in Tweet model to return sorted tweets by latest twwet on top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tweets
   resources :sessions
   resources :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701185301) do
+ActiveRecord::Schema.define(version: 20170701204823) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
